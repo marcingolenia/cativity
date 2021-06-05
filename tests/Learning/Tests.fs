aa7244873385655680b3d4b512da17cb2c238d07
Marcin 👾 Golenia <mgolenia@cybervadis.com>
2021-06-05 16:18:37 +0200

    subscriptions tests

--- tests/Learning/Tests.fs
+++ tests/Learning/Tests.fs
@@ -1,6 +1,8 @@
 module Tests
 
 open System
+open System.Threading
+open System.Threading.Tasks
 open Xunit
 open CosmoStore.Marten
 open CosmoStore
@@ -9,7 +11,7 @@ open FsUnit.Xunit
 type DummyEvent = { IncrementedBy: int }
 
 [<Fact>]
-let ``Appending event to postgres stream`` () =
+let ``Basic event appending to postgres stream with optimistic concurrency versioning`` () =
     // Arrange
     let streamId = "MyAmazingStream"
 
@@ -43,3 +45,74 @@ let ``Appending event to postgres stream`` () =
         |> Async.AwaitTask
         |> Async.RunSynchronously
     newestEvent.Data |> should equal eventWrite.Data
+
+[<Fact>]
+let ``Basic event appending to postgres stream without optimistic concurrency versioning`` () =
+    // Arrange
+    let streamId = "MyAmazingStream"
+
+    let config: Configuration =
+        { Host = "localhost"
+          Username = "postgres"
+          Password = "Secret!Passw0rd"
+          Database = "cativity" }
+
+    let es = EventStore.getEventStore config
+
+    let eventWrite: EventWrite<DummyEvent> =
+        { Id = Guid.NewGuid()
+          CorrelationId = None
+          CausationId = None
+          Name = nameof DummyEvent
+          Data = { IncrementedBy = 4 }
+          Metadata = None }
+    // Act
+    let newestEvent =
+        eventWrite
+        |> es.AppendEvent streamId ExpectedVersion.Any
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
+    // Assert
+    newestEvent.Data |> should equal eventWrite.Data
+    
+[<Fact>]
+let ``Basic event appending to postgres stream and subscription to stream`` () =
+    // Arrange
+    let streamId = "MyAmazingStream"
+    let tcs = TaskCompletionSource<Unit>()
+    let mutable consumedEvents: DummyEvent list = []
+    let eventsCount = 5
+
+    let config: Configuration =
+        { Host = "localhost"
+          Username = "postgres"
+          Password = "Secret!Passw0rd"
+          Database = "cativity" }
+
+    let es = EventStore.getEventStore config
+    use subsription = es.EventAppended.Subscribe(fun evt ->
+        printfn "%A" evt.Data
+        consumedEvents <- evt.Data :: consumedEvents
+        if consumedEvents.Length = eventsCount then tcs.SetResult(())
+        )
+
+    let eventWrite i: EventWrite<DummyEvent> =
+        { Id = Guid.NewGuid()
+          CorrelationId = None
+          CausationId = None
+          Name = nameof DummyEvent
+          Data = { IncrementedBy = i }
+          Metadata = None }
+    // Act
+    [ 1 .. eventsCount ] |> List.iter(fun i ->
+        eventWrite i
+            |> es.AppendEvent streamId ExpectedVersion.Any
+            |> Async.AwaitTask
+            |> Async.RunSynchronously
+            |> ignore
+        Async.Sleep 100 |> Async.RunSynchronously
+        )
+    // Assert
+    tcs.Task |> Async.AwaitTask |> Async.RunSynchronously
+    consumedEvents.Length |> should equal eventsCount
+    printfn "Done!"
